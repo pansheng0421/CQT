cfca4217c77bb1e45d5f8086025b2e206581f9aa
pansheng0421 <ps.pan.sheng@gmail.com>
2018-05-23 23:09:46 +0800

    修改了MACD函数
    修改了MACD函数

--- macd/MACD.py
+++ macd/MACD.py
@@ -2,8 +2,9 @@ from CsvRead import CsvReader
 from CsvRead import CsvWriter
 
 
-def modifyData(result,interSec):
+def modifyData(result):
 	
+	interSec = 60;
 	resultUse= {};
 	for res in result:
 
@@ -79,29 +80,84 @@ def MacdCalc(resultUse,interSec):
 	CsvWriter('data1.csv',resultMacd)
 
 	print 'macd calc done'
-	
 
-def btcControl():
+def MacdHead(result):
+	# calc first line macd
+	resultMacd = {};
+	resultMacd['price'] = result['price'];
+	resultMacd['ema12'] = 0;
+	resultMacd['ema26'] = 0;
+	resultMacd['diff'] = 0;
+	resultMacd['dea'] = 0;
+	resultMacd['macd'] = 0;
+	return resultMacd
+
+def MacdSeconline(lastresult,result):
+
+
+	LastDayEma12 = lastresult['ema12'];
+	LastDayEma26 = lastresult['ema26'];
+	LastDea = lastresult['dea'];
+	LastPrice = lastresult['price'];
+
+	resultMacd = {};
+	price = result['price'];
+	resultMacd['price'] = price;
+	resultMacd['ema12'] = LastPrice + (price - LastPrice)*2/13;
+	resultMacd['ema26'] = LastPrice + (price - LastPrice)*2/27;
+	resultMacd['diff'] = resultMacd['ema12'] - resultMacd['ema26'] ;
+	resultMacd['dea'] = LastDea*8/10 + resultMacd['diff'] *2/10 ;
+	resultMacd['macd'] = 2 * (resultMacd['diff'] - resultMacd['dea']);
+	
+	return resultMacd
+
+def MacdNomalCalc(lastresult,result):
+
+	LastDayEma12 = lastresult['ema12'];
+	LastDayEma26 = lastresult['ema26'];
+	LastDea = lastresult['dea'];
+	LastPrice = lastresult['price'];
+
+	resultMacd = {};
+	price = result['price'];
+	resultMacd['price'] = price;
+	resultMacd['ema12'] = LastDayEma12 * 11/13 + price *2/13;
+	resultMacd['ema26'] = LastDayEma26 * 25/27 + price *2/27;
+	resultMacd['diff'] = resultMacd['ema12'] - resultMacd['ema26'] ;
+	resultMacd['dea'] = LastDayDea*8/10 + resultMacd['diff'] *2/10 ;
+	resultMacd['macd'] = 2 * (resultMacd['diff'] - resultMacd['dea']);
+
+	return resultMacd
+
+
+
+
+def TradeSimu():
 
 	# read from csv
 	result = CsvReader("data.csv");
 
 	# modify result
-	interSec = 60;
-	resultUse = modifyData(result,interSec);
+	resultUse = modifyData(result);
 
+	
+	#resultMacd = MacdCalc(resultUse,60);
+	#CsvWriter("data1.csv",resultMacd);
+
+
+	'''
 	resultMacd = MacdCalc(resultUse,interSec);
 	
 	moneyAmount = 10000;
 	buySellState = 0;
-
+	'''
 	
 
 
 
 
 if __name__=="__main__":   
-	btcControl()
+	TradeSimu()
 	pass
     
 	
