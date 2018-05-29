f7a4ea9eedc2e14e6937bf65ee2c38b9c1dda824
pansheng0421 <ps.pan.sheng@gmail.com>
2018-05-29 21:24:37 +0800

    上传counter第二版
    上传counter第二版

--- CoinCounter/CoinCounter.py
+++ CoinCounter/CoinCounter.py
@@ -1,80 +1,98 @@
 import urllib,urllib2
 import time
 import json  
+import csv
 
 def sleeptime(hour,min,sec):
 	return hour*3600 + min*60 + sec;
 
 def getCoinDataFromWeb():
 	# getdata
-	#url='https://api.coinmarketcap.com/v2/ticker/'+str(id)+'/';
 	url='https://api.coinmarketcap.com/v2/listings/';
 	req = urllib2.Request(url)
 	res = urllib2.urlopen(req)
 	res = res.read()
 
-	'''
-	index1 = res.find("data", 0, len(res));
-	index2 = res.find("metadata", index1, len(res));
-	res = res[index1:index2-8];
-	'''
-
-	res = json.loads(res) 
-
-
-	print res
-	print isinstance(res,dict);
+	res = json.loads(res);
 	return res
 
-def modifyData(res):
-	# find name
-	index1 = res.find("name", 0, len(res))
-	index2 = res.find("\",", index1, len(res))
-	name = res[index1+8:index2]
-	#	print name
+def GetCoinData(CoinId):
+	# getdata
+
+
+	url='https://api.coinmarketcap.com/v2/ticker/'+str(CoinId)+'/';
 	
-	# find price
-	index1 = res.find("price", 0, len(res))
-	index2 = res.find(",", index1, len(res))
-	price = res[index1+8:index2]
-	#	print price
-
-	# find price
-	index1 = res.find("symbol", 0, len(res))
-	index2 = res.find("\"", index1+10, len(res))
-	symbol = res[index1+10:index2]
-	# print symbol
-
-
-	# find time
-	index = res.find("timestamp", 0, len(res))
-	timestamp = int(res[index+12:index+22])
-	#	print(timestamp)
-	time_local = time.localtime(timestamp)
-	dt = time.strftime("%Y-%m-%d %H:%M:%S",time_local)
-	#	print dt
 	
-	data = {}
-	data['name'] = name
-	data['price'] = price
-	data['time'] = dt
-	data['timestamp'] = timestamp
-	data['symbol'] = symbol
+	#url='https://api.coinmarketcap.com/v2/ticker/1/'
+	req = urllib2.Request(url)
+	res = urllib2.urlopen(req)
+	res = res.read()
 
-	print(data['time']),
-	print(' '*5),
-	print(data['symbol']),
-	print(' '*5),
-	print(data['price'])
+	res = json.loads(res);
 
-	return data
+	CoinCurData = {};
+	CoinCurData['id']=res['data']['id'];
+	CoinCurData['name']=res['data']['name'];
+	CoinCurData['symbol']=res['data']['symbol'];
+	CoinCurData['website_slug']=res['data']['website_slug'];
+	CoinCurData['rank']=res['data']['rank'];
+	CoinCurData['circulating_supply']=res['data']['circulating_supply'];
+	CoinCurData['total_supply']=res['data']['total_supply'];
+	CoinCurData['max_supply']=res['data']['max_supply'];
+	CoinCurData['price']=res['data']['quotes']['USD']['price'];
+	CoinCurData['volume_24h']=res['data']['quotes']['USD']['volume_24h'];
+	CoinCurData['market_cap']=res['data']['quotes']['USD']['market_cap'];
+	CoinCurData['percent_change_1h']=res['data']['quotes']['USD']['percent_change_1h'];
+	CoinCurData['percent_change_24h']=res['data']['quotes']['USD']['percent_change_24h'];
+	CoinCurData['percent_change_7d']=res['data']['quotes']['USD']['percent_change_7d'];
+	CoinCurData['last_updated']=res['data']['last_updated'];
+
+	return	CoinCurData
+
+def modifyCoinData(res):
+	
+	resUse = res['data'];
+	CoinData = {}
+	while len(resUse):
+		PreCoinData = resUse.pop();
+		key = str(PreCoinData['symbol']);
+		CoinData[key] = {};
+		CoinData[key]['name'] = str(PreCoinData['name']);
+		CoinData[key]['website_slug'] = str(PreCoinData['website_slug']);
+		CoinData[key]['id'] = int(PreCoinData['id']);
+
+	return CoinData
+
+def CsvReader(file):
+	
+	csvFile = open(file, "r")
+	reader = csv.reader(csvFile)
+
+	
+	result = {}
+	for item in reader:
+	    result[item[0]] = {};
+	    result[item[0]]['num'] = float(item[1]);
+
+	csvFile.close()
+	print result
+	print 'read done'
+	return result
 	
 
 def CoinCounter():
 
 	second1 = sleeptime(0,5,0);
 	second2 = sleeptime(0,0,1);
-	res=getCoinDataFromWeb();
+	res = getCoinDataFromWeb();
+	CoinData = modifyCoinData(res);
+	CoinAcount = CsvReader('data.csv');
+	for key in CoinAcount.keys():
+		CoinId = CoinData[key]['id'];
+		CoinCurData = GetCoinData(CoinId);
+		CoinAcount[key].update(CoinCurData);
+		print CoinAcount[key]
+
 
 
 	#CoinData = {};
